c5b29764625307d657e03e06862cc82192e0ba04
Satyam Pandey <ofcsatyam007@gmail.com>
2025-07-15 20:06:34 +0530

    Various types of functions along with practice questions

--- Functions/Day2.py
+++ Functions/Day2.py
@@ -22,10 +22,10 @@ def get_exp(base, exp):
 #calling the function
 print(get_exp(exp=10,base=2))
 
-#Python Function within Functions:
-#A function that is defined inside another function is known as the inner function or nested function. Nested functions can access variables of the enclosing scope. Inner functions are used so that they can be protected from everything happening outside the function.
+# #Python Function within Functions:
+# #A function that is defined inside another function is known as the inner function or nested function. Nested functions can access variables of the enclosing scope. Inner functions are used so that they can be protected from everything happening outside the function.
 
-#Example:
+# #Example:
 def func1(name):
     print(name)
     age = 20
@@ -37,3 +37,32 @@ func1("Satyam")
 
 
 #Practice Question of above mentioned topics:
+#Ques 1: Write a function to calculate the square of a number?
+def get_a_square(n):
+    result = n*n
+    return result
+n = int(input("Enter a number:"))
+print(get_a_square(n))
+
+# Ques 2: Create a function to reverse a string?
+def reverse_a_string(s):
+    result = ""
+    for char in s:
+        result= char + result
+    print(result)
+s =  str(input("Enter a string type name :"))
+reverse_a_string(s)
+
+#Ques 3: Check a string is a palindrome or not?
+
+def isPalindrome():
+    s = str(input("Enter a string: "))
+    res = ""
+    for char in s:
+        res = char + res
+    print(res) #To check that the string is reversed or not
+    if(s==res):
+        print("Yes")
+    else:
+        print("No")
+isPalindrome()
